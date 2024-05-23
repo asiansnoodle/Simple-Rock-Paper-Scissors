66d38522d1d250c61fc1be279ceae41de63902c5
Noodles <adrianalexis624@gmail.com>
2024-05-22 20:03:05 -0500

    Completed function to generate computer choice
    completed getComputerChoice - returns rock/paper/scissors based on random number generated. Serves as computer's choice

--- game-logic.js
+++ game-logic.js
@@ -1,1 +1,23 @@
 console.log("Hello, World");
+
+function getComputerChoice(){
+    // create logic for computer to select rock, paper or scissors
+    // 1) generate a random number between 0-2 and store that number in a variable
+    // 0 = rock | 1 = paper | 2 = scissors
+    // 3) create an if-else branch to return the selection of the computer
+
+    // step 1:
+    let rng = Math.floor(Math.random() * 3);
+    
+    // step 2:
+    if (rng == 0){
+        return "rock";
+    }
+    else if (rng == 1){
+        return "paper";
+    }
+    else if (rng == 2){
+        return "scissors";
+    }
+
+}
